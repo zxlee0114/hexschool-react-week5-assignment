327e8b3b13181fb48181c1ac932b0b78d3afb74f
zxlee0114 <napoleon.lee0114@gmail.com>
2025-03-06 17:37:03 +0800

    feat: 放上作業模板

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,292 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
+import { Modal } from "bootstrap";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const API_PATH = import.meta.env.VITE_API_PATH;
 
 function App() {
-  return <></>;
+  const [products, setProducts] = useState([]);
+  const [tempProduct, setTempProduct] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(`${BASE_URL}/v2/api/${API_PATH}/products`);
+        setProducts(res.data.products);
+      } catch (error) {
+        alert("取得產品失敗");
+      }
+    };
+    getProducts();
+  }, []);
+
+  const productModalRef = useRef(null);
+  useEffect(() => {
+    new Modal(productModalRef.current, { backdrop: false });
+  }, []);
+
+  const openModal = () => {
+    const modalInstance = Modal.getInstance(productModalRef.current);
+    modalInstance.show();
+  };
+
+  const closeModal = () => {
+    const modalInstance = Modal.getInstance(productModalRef.current);
+    modalInstance.hide();
+  };
+
+  const handleSeeMore = (product) => {
+    setTempProduct(product);
+    openModal();
+  };
+
+  const [qtySelect, setQtySelect] = useState(1);
+
+  return (
+    <div className="container">
+      <div className="mt-4">
+        <table className="table align-middle">
+          <thead>
+            <tr>
+              <th>圖片</th>
+              <th>商品名稱</th>
+              <th>價格</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td style={{ width: "200px" }}>
+                  <img
+                    className="img-fluid"
+                    src={product.imageUrl}
+                    alt={product.title}
+                  />
+                </td>
+                <td>{product.title}</td>
+                <td>
+                  <del className="h6">原價 {product.origin_price} 元</del>
+                  <div className="h5">特價 {product.origin_price}元</div>
+                </td>
+                <td>
+                  <div className="btn-group btn-group-sm">
+                    <button
+                      onClick={() => handleSeeMore(product)}
+                      type="button"
+                      className="btn btn-outline-secondary"
+                    >
+                      查看更多
+                    </button>
+                    <button type="button" className="btn btn-outline-danger">
+                      加到購物車
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <div
+          ref={productModalRef}
+          style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
+          className="modal fade"
+          id="productModal"
+          tabIndex="-1"
+        >
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h2 className="modal-title fs-5">
+                  產品名稱：{tempProduct.title}
+                </h2>
+                <button
+                  onClick={closeModal}
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <img
+                  src={tempProduct.imageUrl}
+                  alt={tempProduct.title}
+                  className="img-fluid"
+                />
+                <p>內容：{tempProduct.content}</p>
+                <p>描述：{tempProduct.description}</p>
+                <p>
+                  價錢：{tempProduct.price}{" "}
+                  <del>{tempProduct.origin_price}</del> 元
+                </p>
+                <div className="input-group align-items-center">
+                  <label htmlFor="qtySelect">數量：</label>
+                  <select
+                    value={qtySelect}
+                    onChange={(e) => setQtySelect(e.target.value)}
+                    id="qtySelect"
+                    className="form-select"
+                  >
+                    {Array.from({ length: 10 }).map((_, index) => (
+                      <option key={index} value={index + 1}>
+                        {index + 1}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-primary">
+                  加入購物車
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="text-end py-3">
+          <button className="btn btn-outline-danger" type="button">
+            清空購物車
+          </button>
+        </div>
+
+        <table className="table align-middle">
+          <thead>
+            <tr>
+              <th></th>
+              <th>品名</th>
+              <th style={{ width: "150px" }}>數量/單位</th>
+              <th className="text-end">單價</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>
+                <button type="button" className="btn btn-outline-danger btn-sm">
+                  x
+                </button>
+              </td>
+              <td></td>
+              <td style={{ width: "150px" }}>
+                <div className="d-flex align-items-center">
+                  <div className="btn-group me-2" role="group">
+                    <button
+                      type="button"
+                      className="btn btn-outline-dark btn-sm"
+                    >
+                      -
+                    </button>
+                    <span
+                      className="btn border border-dark"
+                      style={{ width: "50px", cursor: "auto" }}
+                    ></span>
+                    <button
+                      type="button"
+                      className="btn btn-outline-dark btn-sm"
+                    >
+                      +
+                    </button>
+                  </div>
+                  <span className="input-group-text bg-transparent border-0">
+                    unit
+                  </span>
+                </div>
+              </td>
+              <td className="text-end">單項總價</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan="3" className="text-end">
+                總計：
+              </td>
+              <td className="text-end" style={{ width: "130px" }}></td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+
+      <div className="my-5 row justify-content-center">
+        <form className="col-md-6">
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="form-control"
+              placeholder="請輸入 Email"
+            />
+
+            <p className="text-danger my-2"></p>
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              收件人姓名
+            </label>
+            <input
+              id="name"
+              className="form-control"
+              placeholder="請輸入姓名"
+            />
+
+            <p className="text-danger my-2"></p>
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="tel" className="form-label">
+              收件人電話
+            </label>
+            <input
+              id="tel"
+              type="text"
+              className="form-control"
+              placeholder="請輸入電話"
+            />
+
+            <p className="text-danger my-2"></p>
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="address" className="form-label">
+              收件人地址
+            </label>
+            <input
+              id="address"
+              type="text"
+              className="form-control"
+              placeholder="請輸入地址"
+            />
+
+            <p className="text-danger my-2"></p>
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="message" className="form-label">
+              留言
+            </label>
+            <textarea
+              id="message"
+              className="form-control"
+              cols="30"
+              rows="10"
+            ></textarea>
+          </div>
+          <div className="text-end">
+            <button type="submit" className="btn btn-danger">
+              送出訂單
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
