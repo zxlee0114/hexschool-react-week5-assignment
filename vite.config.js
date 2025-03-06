d18416fb89bdf09eda29407af35a7445729c3ba1
zxlee0114 <napoleon.lee0114@gmail.com>
2025-03-06 17:17:33 +0800

    feat: gh-pages 部屬設置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/hexshool-react-week5-assignment/"
+      : "/",
   plugins: [react()],
-})
+});
