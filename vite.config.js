8e95d9714551b5aa055ad3d675a9159dbbbe6c07
zxlee0114 <napoleon.lee0114@gmail.com>
2025-03-07 02:45:58 +0800

    fix: 路徑問題

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base:
     process.env.NODE_ENV === "production"
-      ? "/hexshool-react-week6-assignment/"
+      ? "/hexshool-react-week5-assignment/"
       : "/",
   plugins: [react()],
 });
